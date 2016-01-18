3223e4f67ec21f6ac7588846a7cc4ad7c56d2be2
Den Odell <denodell@me.com>
2016-01-18 12:31:53 +0100

    Supporting additional CSS class names
    Allowing additional className and optionClassName props to be applied as CSS class names to the data list and data list options, respectively.

--- src/components/DataList.js
+++ src/components/DataList.js
@@ -10,19 +10,21 @@ export default class DataList extends React.Component {
                         index={index} 
                         useNative={this.props.useNative} 
                         selected={this.props.selected == index} 
-                        select={this.props.select} />
+                        select={this.props.select}
+                        className={this.props.optionClassName} />
         })
         var containerStyle = {}
+        var className = 'react-datalist' + (this.props.className ? ' ' + this.props.className : '')
         if (!this.props.useNative) {
             if (this.props.hide) containerStyle.display = 'none'
             else if (this.props.options.length == 0) containerStyle.display = 'none'
             else containerStyle.display = 'block'
         }
         if (this.props.useNative) return (
-            <datalist id={this.props.id} className={"react-datalist"}>{options}</datalist>
+            <datalist id={this.props.id} className={className}>{options}</datalist>
         )
         return (
-            <div id={this.props.id} className="react-datalist" style={containerStyle}>{options}</div>
+            <div id={this.props.id} className={className} style={containerStyle}>{options}</div>
         )
     }
 }
