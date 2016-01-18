45351e84b1185c84255c5e5bd40110427bc3a7a4
Den Odell <denodell@me.com>
2016-01-18 12:33:50 +0100

    Supporting custom CSS class names
    Allowing optional className and selectedClassName properties.

--- src/components/DataListOption.js
+++ src/components/DataListOption.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default class DatalistOption extends React.Component {
     render() {
-        var classes = 'react-datalist-option'
-        if (this.props.selected) classes += ' react-datalist-option-selected'
+        var classes = 'react-datalist-option' + (this.props.className ? ' ' + this.props.className : '')
+        if (this.props.selected) classes += ' react-datalist-option-selected' + (this.props.selectedClassName ? ' ' + this.props.selectedClassName : '')
         if (this.props.useNative) return (
-            <option value={this.props.option} />
+            <option className={classes} value={this.props.option} />
         )
         return (
             <div className={classes} onClick={this.handleClick.bind(this)}>{this.props.option}</div>
